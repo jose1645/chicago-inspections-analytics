11399f40f541f8e6208ee61590689602ec26d8a0
Tono <tono2014q@gmail.com>
2024-11-11 21:18:29 -0600

    asdas

--- scripts/ingesta.py
+++ scripts/ingesta.py
@@ -4,45 +4,79 @@ import pandas as pd
 import boto3
 from sodapy import Socrata
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+s3_bucket = os.getenv("S3_BUCKET_NAME")
+socrata_username = os.getenv("SOCRATA_USERNAME")
+socrata_password = os.getenv("SOCRATA_PASSWORD")
+socrata_app_token = os.getenv("SOCRATA_APP_TOKEN")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+# Validar que las variables de entorno esenciales estén configuradas
+if not all([s3_bucket, socrata_app_token, aws_access_key_id, aws_secret_access_key]):
+    raise ValueError("Algunas variables de entorno esenciales no están configuradas. Revisa el archivo .env.")
+
+# Función para verificar si existe el archivo Pickle
 def verificar_archivo(pickle_file_name):
     return os.path.exists(pickle_file_name)
 
+# Función para cargar datos desde el archivo Pickle
 def cargar_datos(pickle_file_name):
     with open(pickle_file_name, 'rb') as f:
         return pickle.load(f)
 
+# Función para obtener la última fecha de inspección en los datos
 def obtener_ultimo_inspection_date(data):
     if 'inspection_date' in data.columns:
         return data['inspection_date'].max()
     else:
         raise ValueError("La columna 'inspection_date' no existe en los datos.")
 
-def subir_a_s3(file_name, bucket_name, object_name):
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+# Función para obtener un cliente de la API de Socrata
+def get_client():
+    return Socrata('data.cityofchicago.org', socrata_app_token, socrata_username, socrata_password)
+
+# Función para crear el recurso de S3
+def get_s3_resource():
+    return boto3.resource(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
     )
-    s3_client.upload_file(file_name, bucket_name, object_name)
-    print(f"Datos subidos a S3 en el bucket '{bucket_name}' con el nombre '{object_name}'.")
 
+# Función para subir datos al bucket de S3
+def guardar_ingesta(bucket_name, ruta, data_frame):
+    s3 = get_s3_resource()
+    pickle_data = pickle.dumps(data_frame)  # Convertir a Pickle
+    s3.Object(bucket_name, ruta).put(Body=pickle_data)
+    print(f"Datos guardados en {bucket_name}/{ruta}")
+
+# Función para la ingesta inicial
+def ingesta_inicial(client, limit=300000):
+    print("Realizando la ingesta de datos inicial desde la API...")
+    results = client.get("4ijn-s7e5", limit=limit)
+    return pd.DataFrame.from_records(results)
+
+# Función para la ingesta consecutiva de datos
+def ingesta_consecutiva(client, fecha_inicio, limit=1000):
+    print("Realizando la ingesta de datos incrementales desde la API...")
+    query = f"inspection_date >= '{fecha_inicio}'"
+    results = client.get("4ijn-s7e5", where=query, limit=limit)
+    return pd.DataFrame.from_records(results)
+
+# Función principal para la ingesta y almacenamiento
 def ingest_data():
-    s3_bucket = os.getenv("S3_BUCKET_NAME")
-    s3_object_name = "data/ingested_data.pkl"
-    socrata_username = os.getenv("SOCRATA_USERNAME")
-    socrata_password = os.getenv("SOCRATA_PASSWORD")
-    socrata_app_token = os.getenv("SOCRATA_APP_TOKEN")
-
-    if not s3_bucket:
-        raise ValueError("El nombre del bucket S3 no está definido en las variables de entorno.")
-
+    # Nombre del archivo Pickle local
     pickle_file_name = "ingested_data.pkl"
-    new_data = pd.DataFrame()
+    client = get_client()
+    fecha_hoy = datetime.today().strftime('%Y-%m-%d')
 
-    # Cliente de la API de Socrata
-    client = Socrata('data.cityofchicago.org', socrata_app_token, socrata_username, socrata_password)
-
+    # Verificar si es ingesta inicial o consecutiva
     if verificar_archivo(pickle_file_name):
         existing_data = cargar_datos(pickle_file_name)
         existing_data.columns = existing_data.columns.str.strip().str.lower()
@@ -51,37 +85,38 @@ def ingest_data():
         ultimo_inspection_date = obtener_ultimo_inspection_date(existing_data)
         buffer_date = (datetime.strptime(ultimo_inspection_date, '%Y-%m-%d') - timedelta(days=2)).strftime('%Y-%m-%d')
 
-        print("Realizando la ingesta de datos incrementales desde la API...")
-        results = client.get("4ijn-s7e5", where=f"inspection_date >= '{buffer_date}'", limit=30000)
-
-        # Convertir resultados a DataFrame
-        new_data = pd.DataFrame.from_records(results)
+        # Realizar ingesta consecutiva
+        new_data = ingesta_consecutiva(client, buffer_date)
         new_data.columns = new_data.columns.str.strip().str.lower()
-        
+
         # Combinar los datos existentes y nuevos, eliminando duplicados
         combined_data = pd.concat([existing_data, new_data]).drop_duplicates().dropna()
 
+        # Ruta para la ingesta consecutiva en S3
+        s3_object_name = f"ingesta/consecutiva/inspecciones-consecutivas-{fecha_hoy}.pkl"
+    
     else:
-        print("El archivo no existe. Realizando la ingesta de datos inicial desde la API...")
-        results = client.get("4ijn-s7e5", limit=10000)
-
-        # Convertir resultados a DataFrame
-        combined_data = pd.DataFrame.from_records(results)
+        # Realizar ingesta inicial
+        combined_data = ingesta_inicial(client)
         combined_data.columns = combined_data.columns.str.strip().str.lower()
+
+        # Ruta para la ingesta inicial en S3
+        s3_object_name = f"ingesta/inicial/inspecciones-historicas-{fecha_hoy}.pkl"
 
     # Verificar si 'inspection_date' está en las columnas antes de ordenar
     if 'inspection_date' in combined_data.columns:
         combined_data.sort_values(by='inspection_date', ascending=False, inplace=True)
     else:
-        print("La columna 'inspection_date' no se encuentra en combined_data")
         raise KeyError("La columna 'inspection_date' no está presente en los datos.")
 
     # Guardar el DataFrame combinado en Pickle
     with open(pickle_file_name, 'wb') as f:
         pickle.dump(combined_data, f)
-
+    
     print(f"Datos descargados y guardados en {pickle_file_name}.")
-    subir_a_s3(pickle_file_name, s3_bucket, s3_object_name)
+    
+    # Guardar en S3
+    guardar_ingesta(s3_bucket, s3_object_name, combined_data)
 
 if __name__ == "__main__":
     ingest_data()
