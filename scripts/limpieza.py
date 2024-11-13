3660d3a62c4ba373d12a80c77bee277a7003d6af
Antonio <joseantonioalejoramos@outlook.com>
2024-11-13 11:10:43 -0600

    Initial commit

--- scripts/limpieza.py
+++ scripts/limpieza.py
@@ -15,7 +15,7 @@ log_handler = TimedRotatingFileHandler(log_filename, when="M", interval=1, backu
 log_handler.suffix = "%Y-%m"
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
     handlers=[log_handler]
 )
 
@@ -35,53 +35,83 @@ s3 = boto3.client(
 )
 
 def cargar_datos_s3(bucket, ruta):
+    logging.info(f"Cargando archivos desde {bucket}/{ruta}")
     archivos = []
-    response = s3.list_objects_v2(Bucket=bucket, Prefix=ruta)
-    for obj in response.get('Contents', []):
-        if obj['Key'].endswith('.pkl'):
-            archivos.append(obj)
+    try:
+        response = s3.list_objects_v2(Bucket=bucket, Prefix=ruta)
+        for obj in response.get('Contents', []):
+            if obj['Key'].endswith('.pkl'):
+                archivos.append(obj)
+        logging.info(f"{len(archivos)} archivos encontrados.")
+    except Exception as e:
+        logging.error(f"Error al cargar archivos desde S3: {e}")
     return archivos
 
 def descargar_archivo_s3(bucket, archivo_obj):
     archivo = archivo_obj['Key']
-    archivo_obj_data = s3.get_object(Bucket=bucket, Key=archivo)
-    data = pickle.load(archivo_obj_data['Body'])
-    etag = archivo_obj['ETag'].strip('"')
-    return data, etag
+    logging.info(f"Descargando archivo {archivo} de S3.")
+    try:
+        archivo_obj_data = s3.get_object(Bucket=bucket, Key=archivo)
+        data = pickle.load(archivo_obj_data['Body'])
+        etag = archivo_obj['ETag'].strip('"')
+        logging.info(f"Archivo {archivo} descargado con éxito.")
+        return data, etag
+    except Exception as e:
+        logging.error(f"Error al descargar el archivo {archivo} de S3: {e}")
+        return None, None
 
 def transformar_ingesta(data):
+    logging.info("Transformando datos de ingesta.")
     df = pd.DataFrame(data)
     return df
 
 def faltantes(df):
-    print(df.isnull().sum())
+    logging.info("Verificando valores faltantes.")
+    missing_data = df.isnull().sum()
+    logging.info(f"Valores faltantes por columna:\n{missing_data}")
 
 def elimina_faltantes_latitud_longitud(df, columnas):
-    return df.dropna(subset=columnas)
+    logging.info(f"Eliminando filas con valores nulos en las columnas {columnas}.")
+    before_rows = len(df)
+    df = df.dropna(subset=columnas)
+    after_rows = len(df)
+    logging.info(f"Filas eliminadas: {before_rows - after_rows}")
+    return df
 
 def imputar_faltantes(df, columna, valor_imputar):
+    logging.info(f"Imputando valores faltantes en la columna {columna}.")
     df[columna].fillna(valor_imputar, inplace=True)
     return df
 
 def transformar_enteros(df, columnas):
+    logging.info(f"Transformando columnas {columnas} a enteros.")
     for columna in columnas:
         df[columna] = df[columna].astype('Int64')
     return df
 
 def transformar_flotantes(df, columnas):
+    logging.info(f"Transformando columnas {columnas} a flotantes.")
     for columna in columnas:
         df[columna] = df[columna].astype(float)
     return df
 
 def transformar_fechas(df, columnas):
+    logging.info(f"Transformando columnas {columnas} a fechas.")
     for columna in columnas:
         df[columna] = pd.to_datetime(df[columna], errors='coerce')
     return df
 
 def existe_archivo_limpio(bucket, ruta_limpia, etag):
     archivo_limpio = f"{ruta_limpia}/datos_limpios/datos_limpios_{datetime.today().strftime('%Y-%m-%d')}_{etag}.pkl"
-    response = s3.list_objects_v2(Bucket=bucket, Prefix=archivo_limpio)
-    return 'Contents' in response
+    try:
+        response = s3.list_objects_v2(Bucket=bucket, Prefix=archivo_limpio)
+        if 'Contents' in response:
+            logging.info(f"El archivo limpio {archivo_limpio} ya existe en S3.")
+            return True
+        return False
+    except Exception as e:
+        logging.error(f"Error al verificar la existencia del archivo limpio: {e}")
+        return False
 
 def guardar_datos_s3(bucket, ruta, df, etag):
     fecha = datetime.today().strftime('%Y-%m-%d')
@@ -93,8 +123,11 @@ def guardar_datos_s3(bucket, ruta, df, etag):
         temp_file_path = temp_file.name  # Ruta al archivo temporal
     
     # Subir directamente el archivo temporal a S3
-    s3.upload_file(temp_file_path, bucket, archivo)
-    logging.info(f"Archivo limpio guardado en S3: {archivo}")
+    try:
+        s3.upload_file(temp_file_path, bucket, archivo)
+        logging.info(f"Archivo limpio guardado en S3: {archivo}")
+    except Exception as e:
+        logging.error(f"Error al guardar el archivo limpio en S3: {e}")
 
 def procesar_archivos(bucket, rutas):
     ruta_limpia = 'datos_limpios'
@@ -102,6 +135,9 @@ def procesar_archivos(bucket, rutas):
         archivos = cargar_datos_s3(bucket, ruta)
         for archivo_obj in archivos:
             data, etag = descargar_archivo_s3(bucket, archivo_obj)
+            
+            if data is None:
+                continue
             
             # Validación: si el archivo limpio ya existe, salta a la siguiente iteración
             if existe_archivo_limpio(bucket, ruta_limpia, etag):
@@ -128,5 +164,6 @@ if __name__ == "__main__":
     logging.info("Inicio de la ejecución de limpieza diaria")
     procesar_archivos(s3_bucket, rutas_ingesta)
         
-        # Espera de un día (24 horas)
+    # Espera de un día (24 horas)
     logging.info("Limpieza completada. Esperando 24 horas para la próxima ejecución.")
+    time.sleep(86400)  # Espera de 24 horas (86400 segundos)
