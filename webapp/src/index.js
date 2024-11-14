ce06163c206d0f77a944c7f8806bfd29dd389a4e
Antonio <joseantonioalejoramos@outlook.com>
2024-11-14 12:02:04 -0600

    Initial commit

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // A partir de React 18, usamos `react-dom/client` para crear el root
 //import './index.css'; // Aquí puedes importar tu archivo de estilos globales
 import App from './App'; // El componente principal
-import reportWebVitals from './reportWebVitals'; // (Opcional) para medir el rendimiento
+//import reportWebVitals from './reportWebVitals'; // (Opcional) para medir el rendimiento
 
 // Crear el "root" del DOM donde se montará la aplicación React
 const root = ReactDOM.createRoot(document.getElementById('root'));
