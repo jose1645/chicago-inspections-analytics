a3bcd7f096d1cabdd37584bcdf1abc3b0d9106ed
Antonio <joseantonioalejoramos@outlook.com>
2024-11-14 11:58:19 -0600

    Initial commit

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // A partir de React 18, usamos `react-dom/client` para crear el root
-import './index.css'; // Aquí puedes importar tu archivo de estilos globales
+//import './index.css'; // Aquí puedes importar tu archivo de estilos globales
 import App from './App'; // El componente principal
 import reportWebVitals from './reportWebVitals'; // (Opcional) para medir el rendimiento
 
