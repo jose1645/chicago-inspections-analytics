9484a7a5f1fcc2ef4937f49231d9c3926b167767
Antonio <joseantonioalejoramos@outlook.com>
2024-11-20 12:31:53 -0600

    Cosntruir backend

--- backend_webapp/backend_webapp/settings.py
+++ backend_webapp/backend_webapp/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = '/app/static/'
+STATIC_URL = '/backend-static/'
+STATIC_ROOT = '/usr/share/nginx/html/backend-static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
