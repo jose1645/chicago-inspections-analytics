7d9c7e28aa8137bef24e55e87e35dddcdcc03512
tono <tono2014q@gmail.com>
2024-11-20 23:32:24 -0600

    Mensaje del commit

--- webapp/src/components/Dashboard.js
+++ webapp/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import '../styles/Dashboard.css';
+import ChicagoMap from './ChicagoMap';
 
 const Dashboard = () => {
     const [kpis, setKpis] = useState(null);
@@ -20,8 +21,8 @@ const Dashboard = () => {
                 setKpis(response.data);
 
                 // Cargar el archivo TopoJSON desde utils/chicago.json
-               // const topoResponse = await d3.json('/utils/chicago.json');
-                //setTopoData(topoResponse);
+                const topoResponse = await d3.json('/utils/chicago.json');
+                setTopoData(topoResponse);
 
                 // Extraer ubicaciones de inspección
                 setInspectionLocations(response.data.inspection_locations);
@@ -84,54 +85,5 @@ const Dashboard = () => {
     );
 };
 
-// Componente ChicagoMap
-const ChicagoMap = ({ topoData, inspectionLocations }) => {
-    const svgRef = React.useRef(null);
-
-    useEffect(() => {
-        const width = 960;
-        const height = 600;
-
-        const projection = d3.geoMercator()
-            .scale(100000) // Escala para Chicago
-            .center([-87.6298, 41.8781]) // Centrar en Chicago
-            .translate([width / 2, height / 2]);
-
-        const path = d3.geoPath(projection);
-
-        const svg = d3.select(svgRef.current)
-            .attr("viewBox", [0, 0, width, height])
-            .call(
-                d3.zoom()
-                    .scaleExtent([1, 8])
-                    .on("zoom", (event) => {
-                        g.attr("transform", event.transform);
-                    })
-            );
-
-        const g = svg.append("g");
-
-        // Dibujar límites de Chicago
-        g.append("path")
-            .datum(topojson.feature(topoData, topoData.objects.chicago))
-            .attr("fill", "#ddd")
-            .attr("stroke", "#333")
-            .attr("stroke-linejoin", "round")
-            .attr("d", path);
-
-        // Dibujar ubicaciones de inspección
-        g.selectAll("circle")
-            .data(inspectionLocations)
-            .join("circle")
-            .attr("cx", d => projection([d.longitude, d.latitude])[0])
-            .attr("cy", d => projection([d.longitude, d.latitude])[1])
-            .attr("r", 3)
-            .attr("fill", "blue")
-            .attr("opacity", 0.7);
-
-    }, [topoData, inspectionLocations]);
-
-    return <svg ref={svgRef}></svg>;
-};
 
 export default Dashboard;
