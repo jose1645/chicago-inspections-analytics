ca48366365b593e1e67c392f3371b9bdba8af35a
Antonio <joseantonioalejoramos@outlook.com>
2024-12-06 13:16:33 -0600

    Ajustar la limpieza de los valores nulos

--- webapp/src/components/MLModels.js
+++ webapp/src/components/MLModels.js
@@ -6,8 +6,21 @@ const MLModels = () => (
     <div className="mlmodels">
         <h2>Modelos de Machine Learning</h2>
         <p>Explicación de los modelos y su rendimiento en la predicción...</p>
-        <div className="model-placeholder">[Gráfico de Matriz de Confusión]</div>
-        <div className="model-placeholder">[Curva ROC]</div>
+
+        {/* Mostrar Notebook como HTML */}
+        <div className="notebook-container">
+            <iframe
+                src="/notebooks/Investigar_correlaciones_con_results.html" // Cambia la ruta al archivo HTML de tu notebook
+                title="Notebook de Modelos"
+                style={{
+                    width: '100%',
+                    height: '600px',
+                    border: '1px solid #ddd',
+                    borderRadius: '8px',
+                    overflow: 'hidden',
+                }}
+            ></iframe>
+        </div>
     </div>
 );
 
